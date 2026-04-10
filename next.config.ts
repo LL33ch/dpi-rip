df9524c83b3130999ce2f7861193157278253891
LL33ch <s.garazh2002@gmail.com>
2026-04-10 18:29:23 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/dpi-checker',
   images: {
-    unoptimized: true,
     remotePatterns: [
       { hostname: 'flagcdn.com' },
     ],
