76a56006cfc87cbe8e7eaed0a783c530dd41e043
LL33ch <s.garazh2002@gmail.com>
2026-05-12 03:35:26 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
   images: {
     loader: 'custom',
     loaderFile: './src/shared/lib/image-loader.ts',
